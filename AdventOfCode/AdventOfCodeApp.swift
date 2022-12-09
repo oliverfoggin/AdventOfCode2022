e3bd33415eb7a1451b58e3998386aee470058b79
Oliver Foggin <oliverfoggin@me.com>
2022-12-09 08:34:53 +0000

    Initial Commit

---
+++ AdventOfCode/AdventOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeApp.swift
+//  AdventOfCode
+//
+//  Created by Oliver Foggin on 09/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
